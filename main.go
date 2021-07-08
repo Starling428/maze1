86378afdc4d8e47f47abd19f81d48c6bcbe2a32b
Starling428 <moses.sarkisov@onlinesup.com>
2021-07-08 17:54:44 +0300

    final

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"image/color"
@@ -10,35 +11,100 @@ import (
 	"time"
 )
 
+func drawBlock(imag *image.RGBA, x0 int, y0 int, scale int, col color.Color) {
+	if scale == 1 {
+		imag.Set(x0, y0, col)
+	} else {
+		xt := x0 * scale
+		yt := y0 * scale
+		for i := 0; i < scale; i++ {
+			for j := 0; j < scale; j++ {
+				imag.Set(xt+i, yt+j, col)
+			}
+		}
+	}
+}
+
 func main() {
-	const height, width = 75, 75
+
+	size := flag.Int("size", 201, "Length of the rib of the maze (the maze is a square")
+	scale := flag.Int("scale", 3, "Pixels in one block")
+	startx := flag.Int("startx", 101, "Start point x coordinate")
+	starty := flag.Int("starty", 101, "Start point y coordinate")
+	inversion := flag.Bool("inversion", false, "Invert colors")
+
+	flag.Parse()
+
+	if *scale < 1 {
+		*scale = 2
+	}
+
+	if *size < 5 {
+		*size = 63
+	} else {
+		if *size%2 == 0 {
+			*size++
+		}
+	}
+
+	if *startx < 1 || *startx > *size-2 {
+		*startx = 1
+	}
+	if *starty < 1 || *starty > *size-2 {
+		*starty = 1
+	}
+
+	if *startx%2 == 0 {
+		if *startx >= 2 {
+			*startx--
+		} else {
+			*startx++
+		}
+	}
+	if *starty%2 == 0 {
+		if *starty >= 2 {
+			*starty--
+		} else {
+			*starty++
+		}
+	}
+
+	var linecolor, bgcolor color.Gray16
+
+	if *inversion {
+		linecolor = color.White
+		bgcolor = color.Black
+	} else {
+		linecolor = color.Black
+		bgcolor = color.White
+	}
+
+	var height, width = *size, *size
 	type block struct {
 		//x, y int
 		visited, iswall bool
 	}
-	maze := [height][width]block{}
-	var linecolor, bgcolor color.Gray16
-	linecolor = color.Black
-	bgcolor = color.White
+	maze := [][]block{}
+
+	for i := 0; i < height; i++ {
+		maze = append(maze, []block{})
+		for j := 0; j < width; j++ {
+			maze[i] = append(maze[i], block{})
+		}
+	}
 
 	type Point struct{ X, Y int }
 	var tempBlock Point
 	var stack []Point
 	var stackTemp []Point
 
-	//stack = append(stack, Point{1,7}) // Push
-	//stack = append(stack, Point{2,8})
-	//stack = append(stack, Point{3,9}) // Push
-	//stack = append(stack, Point{4,10})
-	//stack = append(stack, Point{5,11}) // Push
-	//stack = append(stack, Point{6,12})
-
-	A := (height - 1) * (height - 1) / 4
+	Unvisited := (height - 1) * (height - 1) / 4
 	// img creating
-	img := image.NewRGBA(image.Rect(0, 0, height, height))
+	img := image.NewRGBA(image.Rect(0, 0, *scale*height, *scale*height))
 	for y := 1; y <= height; y++ {
 		for x := 1; x <= height; x++ {
-			img.Set(x, y, bgcolor)
+			drawBlock(img, x, y, *scale, bgcolor)
+			//img.Set(x, y, bgcolor)
 		}
 	}
 
@@ -52,39 +118,17 @@ func main() {
 			} else {
 				maze[i][j].iswall = true
 				maze[i][j].visited = true
-				img.Set(i, j, linecolor)
+				//img.Set(i, j, linecolor)
+				drawBlock(img, i, j, *scale, linecolor)
 			}
 		}
 	}
 
-	//text debug
-	//for i:=0; i<height; i++ {
-	//	for j:=0; j<width; j++ {
-	//		if maze[i][j].visited {print(1)} else {print (0)}
-	//	}
-	//	println()
-	//}
-	//for i:=1; i < height; i+=2 {
-	//	for j:=1; j < width; j+=2 {
-	//			maze[i][j].visited=true
-	//			A--
-	//			img.Set(i, j, color.RGBA{R:255, A:255})
-	//	}
-	//}
-
-	//for i:=1; i < height; i+=2 {
-	//	for j:=1; j < width; j+=2 {
-	//		maze[i][j].iswall=true
-	//		A--
-	//		img.Set(i, j, color.RGBA{R:255, A:255})
-	//	}
-	//}
-
-	x0, y0 := 5, 5
+	x0, y0 := *startx, *starty
 	maze[x0][y0].visited = true
-	A = (height - 1) * (height - 1) / 4
+	Unvisited = (height - 1) * (height - 1) / 4
 	rand.Seed(time.Now().UTC().UnixNano())
-	for A > 0 {
+	for Unvisited > 0 {
 		if x0 > 1 {
 			if !maze[x0-2][y0].visited {
 				stackTemp = append(stackTemp, Point{x0 - 2, y0})
@@ -107,15 +151,15 @@ func main() {
 		}
 		if len(stackTemp) != 0 {
 			stack = append(stack, Point{x0, y0})
-			fmt.Println(len(stackTemp), rand.Intn(len(stackTemp)))
 			tempBlock = stackTemp[rand.Intn(len(stackTemp))]
 			stackTemp = stackTemp[:0]
 			maze[(x0+tempBlock.X)/2][(y0+tempBlock.Y)/2].iswall = false
-			img.Set((x0+tempBlock.X)/2, (y0+tempBlock.Y)/2, color.White)
+			//img.Set((x0+tempBlock.X)/2, (y0+tempBlock.Y)/2, color.White)
+			drawBlock(img, (x0+tempBlock.X)/2, (y0+tempBlock.Y)/2, *scale, bgcolor)
 			x0 = tempBlock.X
 			y0 = tempBlock.Y
 			maze[x0][y0].visited = true
-			A--
+			Unvisited--
 		} else if len(stack) != 0 {
 			tempBlock = stack[len(stack)-1]
 			x0 = tempBlock.X
